78eae9325bd81cf31807d41f2c766c12367b2d53
abdalla-amin <abdalla.amin@vodafone.com>
2023-01-10 15:16:41 +0200

    project done

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Calculator from './components/Calculator.vue'
+</script>
+
+<template >
+<div class="body">
+  <Calculator/>
+</div>
+</template>
+
+<style >
+body {
+  background:#31475e;
+}
+</style>
